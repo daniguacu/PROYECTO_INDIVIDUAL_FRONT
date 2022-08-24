0a30f0b933bbdcb977d68d2b5a4aa3c07a0550cd
daniguacu <daniguacu@gmail.com>
2022-08-23 23:47:51 -0500

    create property

--- src/components/Property/PropertyForm.js
+++ src/components/Property/PropertyForm.js
@@ -1,8 +1,43 @@
+import {useParams} from "react-router-dom"
+import { useState,useEffect} from "react";
+import axios from "axios";
 
-const PropertyForm= ({landlordId}) => {
+
+const PropertyForm= () => {
+    const params=useParams()
+    const landlordid=params.landlordId
     
+    const[property,setProperty]=useState()
+    const [address, setAddress] = useState("")
+    const [landlord, setLandlord] = useState("")
     
 
+    const AddProperty= async ()=>{
+        await axios.post(`http://localhost:3000/properties/add`,{address,landlord}).
+        then(response=>{
+          setProperty(response.data)
+          console.log(response.data)
+          setAddress("")
+          
+          
+          
+        }).catch(error=>{
+          console.log(error)
+        })
+    }
+
+            
+      
+    
+    const handleChange=(e)=>{
+      setAddress(e.target.value)
+      
+
+    }
+  useEffect(()=>{
+      setLandlord(landlordid);
+  },[])
+    
     
     
     
@@ -10,7 +45,16 @@ const PropertyForm= ({landlordId}) => {
   return (
     <>        
         
-    <h1>Formulario propiedad </h1>
+        <h1>Formulario Agregar</h1>
+        
+        <label>
+          Dirección
+          
+          <input type='text' value={address} onChange={handleChange} />   
+        </label>
+        
+        <button onClick={AddProperty}>Agregar</button>
+       
         
              
       
